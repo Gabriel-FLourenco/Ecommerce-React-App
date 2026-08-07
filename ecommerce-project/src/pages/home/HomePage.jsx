16f6472aad933c0706a0eda3899fb152df026d19
Gabriel <gabrielflourenco2007@gmail.com>
2026-08-06 21:53:31 -0300

    Async Await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -9,11 +9,13 @@ import React from 'react';
 export function HomePage({cart}) {
   const [products,setProducts] = useState([]);
   
-  useEffect(()=>{
-    axios.get('/api/products')
-    .then((response)=>{
+  useEffect( ()=>{
+    const getHomeData= async ()=>{
+      const response = await axios.get('/api/products')
       setProducts(response.data);
-  });
+    }
+
+    getHomeData();
   },[]);
   
 
--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -7,12 +7,16 @@ import { OrderHeader } from './OrderHeader';
 
 export function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
+
+    
     useEffect(() => {
-        axios.get('/api/orders?expand=products')
-            .then((response) => {
+        const fetchOrdersData = async () =>{
+            const response = await axios.get('/api/orders?expand=products')
                 setOrders(response.data);
-            })
+        };
+        fetchOrdersData();
     }, []);
+
     return (
         <>
             <title>Orders</title>
