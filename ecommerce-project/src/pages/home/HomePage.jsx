9b83e8ae90ebd14a70806cf0d5a431e3387959b1
Gabriel <gabrielflourenco2007@gmail.com>
2026-08-07 21:16:33 -0300

    Update delivery option feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -6,7 +6,7 @@ import { ProductsGrid } from './ProductsGrid';
 import React from 'react';
 
 
-export function HomePage({cart}) {
+export function HomePage({cart,loadCart}) {
   const [products,setProducts] = useState([]);
   
   useEffect( ()=>{
@@ -28,7 +28,7 @@ export function HomePage({cart}) {
 
 
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} loadCart={loadCart}/>
       </div>
     </>
   );
